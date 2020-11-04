af4dcdd84ad5b4d0832c3bad49a0a32e8a71b6af
karola-kasprzak <karola.a.kasprzak@gmail.com>
2020-11-04 12:13:17 +0100

    "script added"

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//global variables:
+const allMessages = [];
+const submitBtn = document.querySelector('#submit');
+
+//classes:
+class Message {
+  constructor(name, email, message, date) {
+    this.name = name,
+      this.email = email,
+      this.message = message,
+      this.date = date
+  }
+};
+
+
+// function declarations:
+function addMessage () {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let date = new Date();
+  let newMsg = new Message(name,email,message,date);
+  allMessages.push(newMsg);
+  console.log("addMessage -> allMessages", allMessages);
+  return; 
+}
+
+function clearInput() {
+  document.querySelector('#name').value = "";
+  document.querySelector('#email').value = "";
+  document.querySelector('#message').value = "";
+};
+
+//scripts:
+submitBtn.addEventListener("click", () => {
+  addMessage()
+  clearInput()
+})
+
